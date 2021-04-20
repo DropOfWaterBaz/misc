914e11ad772c24287d0f936a8506b8ed85735b18
ronanmmurphy <ronan-murphy@outlook.ie>
2021-04-20 08:43:05 +0100

    ronan

--- dbazFYP/home.js
+++ dbazFYP/home.js
@@ -5,8 +5,22 @@ app.controller("QuizController", ['$scope',function($scope){
      window.location = "#/resultsPage.html";
   }
 
-  let EISum, SNSum, TFSum, JPSum = 0; // setting sums from inputs
+  var EISum, SNSum, TFSum, JPSum = 0; // setting sums from inputs
 
   
+  function calculatescore(){
+    var radios = document.getElementsByTagName('input');
+    var value;
+    for (var i = 0; i < radios.length; i++) {
+      
+      if (radios[i].type === 'radio' && radios[i].checked) {
+        // get value, set checked flag or do whatever you need to
+        value = radios[i].value;       
+        console.log(value)
+      }
+    }
+
+    return value;
+  }
 
 }])
